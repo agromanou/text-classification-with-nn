07d9ec942c1652eaa624f1191488c3b90c8c6758
giorgos <perakisgeorgios@gmail.com>
2018-04-15 21:14:09 +0300

    created train_dev_split(). Added tbCallBack to fit(). Added pred_scores to predict() resuults

--- app/model.py
+++ app/model.py
@@ -1,9 +1,10 @@
 import os
 
 import numpy as np
+from keras import callbacks
 from keras import optimizers
 from keras import regularizers
-from keras.models import load_model
+from keras.models import load_model as keras_load_model
 from keras.utils import plot_model as keras_plot_model
 from matplotlib import pyplot as plt
 
@@ -35,9 +36,12 @@ class Model:
         :param decay:
         :param momentum:
         :param kernel_regularization_params:
-        :param validation_size:
         :param epochs:
         :param batch_size:
+        :param validation_size:
+        :param outfile:
+        :param plot_model:
+        :param load_model:
         """
 
         self.model = None
@@ -47,6 +51,13 @@ class Model:
         self.plot_model = plot_model
         self.outfile = outfile
         self.load_model = load_model
+
+        self.X_train = None
+        self.y_train = None
+        self.X_val = None
+        self.y_val = None
+
+        self.load_trained_model()
 
         # restrictions about the optimizers
         self.optimizer = optimizer
@@ -91,10 +102,41 @@ class Model:
             elif kernel_regularization_params[0] == 'l2':
                 self.kernel_regularizer = regularizers.l2(value)
 
-    def load_model(self):
+    def train_dev_split(self, X, y):
+        """
+
+        :param X:
+        :param y:
+        :return:
+        """
+        np.random.seed(200)
+
+        # shuffling the training instances
+        indices = np.arange(X.shape[0])
+        np.random.shuffle(indices)
+        train_data = X[indices]
+        train_labels = y[indices]
+
+        # calculating number of validation examples
+        nb_validation_samples = int(self.validation_size * train_data.shape[0])
+
+        # splitting in training and validation data
+        x_train = train_data[:-nb_validation_samples]
+        y_train = train_labels[:-nb_validation_samples]
+        x_val = train_data[-nb_validation_samples:]
+        y_val = train_labels[-nb_validation_samples:]
+
+        self.X_train = x_train
+        self.y_train = y_train
+        self.X_val = x_val
+        self.y_val = y_val
+
+        return {'x_train': x_train, 'x_val': x_val, 'y_train': y_train, 'y_val': y_val}
+
+    def load_trained_model(self):
         if self.load_model and self.outfile:
             model_path = os.path.join(MODELS_DIR, self.outfile + '.h5')
-            self.model = load_model(model_path)
+            self.model = keras_load_model(model_path)
 
     def build_model(self, input_shape, labels_number):
         """
@@ -107,19 +149,28 @@ class Model:
 
         :return:
         """
+
+        # self.train_dev_split(X=x_train, y=y_train)
+
         input_shape = (x_train.shape[1],)
 
-        self.build_model(input_shape=input_shape,
-                         labels_number=2)
+        self.build_model(input_shape=input_shape, labels_number=2)
 
         print('EPOCHS: {}, BATCH SIZE: {}'.format(self.epochs, self.batch_size))
+
+        tbCallBack = callbacks.TensorBoard(log_dir='./Graph',
+                                           histogram_freq=0,
+                                           write_graph=True,
+                                           write_images=True)
 
         history = self.model.fit(x=x_train,
                                  y=y_train,
                                  epochs=self.epochs,
                                  batch_size=self.batch_size,
-                                 validation_split=self.validation_size,
-                                 verbose=2)
+                                 validation_size=self.validation_size,
+                                 # validation_data=(self.X_val, self.y_val),
+                                 verbose=2,
+                                 callbacks=[tbCallBack])
 
         if self.outfile:
             model_path = os.path.join(MODELS_DIR, self.outfile + '.h5')
@@ -145,10 +196,14 @@ class Model:
                                      verbose=2)
 
         predicted_classes = self.model.predict(X, batch_size=self.batch_size)
-        predicted_classes = list(map(lambda x: 1 if x > 0.5 else 0, list(np.squeeze(predicted_classes))))
+
+        pred_scores = np.squeeze(predicted_classes)
+
+        predicted_classes = list(map(lambda x: 1 if x > 0.5 else 0, list(pred_scores)))
 
         return {'scores': scores,
                 'y_pred': predicted_classes,
+                'y_pred_scores': pred_scores,
                 'y_true': y}
 
     @staticmethod
